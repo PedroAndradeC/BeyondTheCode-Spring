a114936fed242b029dad5c09e82cf399917aa1c9
pedroandradecosta@outlook.com <pedroandradecosta@outlook.com>
2023-07-20 14:36:02 -0300

    primeiro commit

---
+++ src/main/java/com/jornada/beyondthecodeapi/BeyondthecodeApiApplication.java
@@ -0,0 +1,13 @@
+package com.jornada.beyondthecodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeyondthecodeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeyondthecodeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jornada/beyondthecodeapi/BeyondthecodeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jornada.beyondthecodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeyondthecodeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
