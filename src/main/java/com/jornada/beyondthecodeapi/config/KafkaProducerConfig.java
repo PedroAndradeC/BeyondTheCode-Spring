d8e126ae7bef71e93297b3de482e3462adc83c83
Frank <contatoguilhermefrank@gmail.com>
2023-10-15 17:04:59 -0300

    CONFIGURAÇÃO DO KAFKA

---
+++ src/main/java/com/jornada/beyondthecodeapi/config/KafkaProducerConfig.java
@@ -0,0 +1,33 @@
+package com.jornada.beyondthecodeapi.config;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value(value="${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Bean
+    public KafkaTemplate<String, String> config(){
+
+        Map<String, Object> configuracao = new HashMap<>();
+        configuracao.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configuracao.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configuracao.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+
+
+        return new KafkaTemplate<>(new DefaultKafkaProducerFactory<>(configuracao));
+    }
+
+}
---
+++ src/main/java/com/jornada/beyondthecodeapi/controller/EnvioController.java
@@ -0,0 +1,14 @@
+package com.jornada.beyondthecodeapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class EnvioController {
+
+    public void enviarMensagem(String mensagem) {
+
+    }
+
+}
